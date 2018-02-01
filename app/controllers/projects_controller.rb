8a4a58e4524d06586fe6f97bb3213573358da99c
Vitalik Bakanoff <mrmaster@ukr.net>
2018-02-01 20:43:35 +0200

    Projects CRUD

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,43 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.create(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to project_path(@project)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+end
